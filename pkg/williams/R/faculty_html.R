2b065eb199cf1d79415f14076aff0712cb9061c0
aml05williams (Andrew Leeser) <aml05williams@yahoo.com>
2010-07-22 14:28:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/williams@3 15becc7f-e186-455b-ac1d-0edd76190b49

--- pkg/williams/R/faculty_html.R
+++ pkg/williams/R/faculty_html.R
@@ -1,64 +1,78 @@
 ## Read in for year 2000 data (originally in HTML format)
 
-for(j = 1:length(yrs)){
+setwd("c:/Users/Andrew/Desktop/R stuff/faculty project/raw data")
+
+## Establish trim function to remove leading and trailing blanks
+
+.trim <- function(str){
+    names(str) <- c()
+    str <- sub("^ +", "", str)
+    str <- sub(" +$", "", str)
+}
+
+
+alldata <- data.frame()
+
+years <- c(1998:2000)
+
+
+for(j in 1:length(years)){
 
     ## The yrs vector encompasses all the years of data to be run using the
     ## html read-in function
 
-    yrs <- c("9900")
-
     ## Read in the raw data for the current year in the loop
 
-    rawdata <- setup.html.data(yrs,j)
+    current.year <- years[j]
 
-    ## Extract the academic year and format it consistent with PDF data
-
-    acyr <- get.acyear.html(rawdata[["x"]])
+    x <- setup.html.data(current.year)
 
     ## Create a list of departments
 
-    prg <- get.prg(rawdata[["y"]])
+    department <- get.department(x)
 
     ## Extract professor names and titles from each department, and
     ## assign the department information to each professor
 
-    html.info.2000 <- get.info.2000(rawdata[["x"]],rawdata[["y"]])
+    info.html <- get.info.html(x, department, current.year)
+
+    ## Create academic year variable
+
+    acyr <- paste(current.year - 1, current.year, sep = "-")
 
     ## In order to append the HTML data to the PDF data, create blank
     ## fields containing missing values
 
-    year <- rep(acyr, times = length(allprofs))
-    first.name <- rep("N/A", times = length(allprofs))
-    middle.name <- rep("N/A", times = length(allprofs))
-    last.name <- rep("N/A", times = length(allprofs))
-    undergrad.degree <- rep("N/A", times = length(allprofs))
-    undergrad.school <- rep("N/A", times = length(allprofs))
-    undergrad.year <- rep("N/A", times = length(allprofs))
-    grad.degree <- rep("N/A", times = length(allprofs))
-    grad.school <- rep("N/A", times = length(allprofs))
-    grad.year <- rep("N/A", times = length(allprofs))
-    est.age <- rep("N/A", times = length(allprofs))
+    academic.year <- rep(acyr, times = nrow(info.html))
+    first.name <- rep(NA, times = nrow(info.html))
+    middle.name <- rep(NA, times = nrow(info.html))
+    last.name <- rep(NA, times = nrow(info.html))
+    undergrad.degree <- rep(NA, times = nrow(info.html))
+    undergrad.school <- rep(NA, times = nrow(info.html))
+    undergrad.year <- rep(NA, times = nrow(info.html))
+    grad.degree <- rep(NA, times = nrow(info.html))
+    grad.school <- rep(NA, times = nrow(info.html))
+    grad.year <- rep(NA, times = nrow(info.html))
 
     ## Create a dataframe including all fields from both the PDF and
     ## HTML data
 
-    data <- data.frame("academic.year" = year,
-                       "professor" = allprofs,
-                       first.name,
-                       middle.name,
-                       last.name,
-                       undergrad.degree,
-                       undergrad.school,
-                       undergrad.year,
-                       grad.degree,
-                       grad.school,
-                       grad.year,
-                       est.age,
-                       "title" = alltitles,
-                       "department" = alldepts)
+    y <- data.frame(academic.year,
+                    first.name,
+                    middle.name,
+                    last.name,
+                    undergrad.degree,
+                    undergrad.school,
+                    undergrad.year,
+                    grad.degree,
+                    grad.school,
+                    grad.year,
+                    est.age)
 
     ## Append the data to the previously generated dataframe
 
-    alldata <- rbind(alldata, data)
+    z <- cbind(info.html, y)
+
+    alldata <- rbind(alldata, z)
 
 }
---
+++ pkg/williams/R/getprg.R
@@ -0,0 +1,27 @@
+get.department <- function(x){
+
+    ## The purpose of the get.prg function is to extract the
+    ## department names from the faculty data originally formatted in
+    ## HTML. This encompasses academic years 1998 through 2000. It has
+    ## only one input - pos
+
+    ## pos is a previously created character vector which consists of
+    ## extracts from the raw faculty data, where a "(Div" string
+    ## exists. This string identifies all lines in which a department
+    ## name exists
+
+    y <- grep("\\(Div.", x)
+
+    ## One department name is too long and thus the division
+    ## information appears on the next line
+
+    z <- which(.trim(x[y]) == "(Div. II)")
+
+    y[z] <- y[z] - 1
+
+    x[y[z]] <- sub("<", " \\(", x[y[z]])
+
+    return(.trim(substr(x[y], 1, regexpr("\\(", x[y]) - 1)))
+}
+
+
